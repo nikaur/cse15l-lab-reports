2c1588f40f762e6fc0eb1bd3ed19d62db20302c2
nimkaur <67022580+nimkaur@users.noreply.github.com>
2022-09-30 12:23:17 -0700

    new files - lab report 1

---
+++ Random.java
@@ -0,0 +1,9 @@
+class Random {
+    public static void main(String[] args){
+        System.out.println("hi");
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(System.getProperty("user.home"));
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
